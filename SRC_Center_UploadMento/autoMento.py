a19060c3dbdcb20c9ea9089e0b62877ff171d73b
OneCrazyman <cjw7155@naver.com>
2022-07-11 17:01:13 +0900

    SRC근로 멘토업로드

--- SRC_Center_UploadMento/autoMento.py
+++ SRC_Center_UploadMento/autoMento.py
@@ -1,25 +1,32 @@
+#FHD환경 
+#종합정보시스템 조직관리 자동화
 import pyautogui as pag
 import pygetwindow as gw
+import pyperclip
 import pywinauto
-# import mss, cv2
-# import numpy as np
-import random, time
+import time
 import keyboard
 import threading, os
+import time
+from openpyxl import load_workbook
+from openpyxl import Workbook
 
+#윈도우 맨위로 가져오기
 def bring_to_window() :
     win = gw.getWindowsWithTitle('종합정보시스템')[0] 
     if win.isActive == False:
         pywinauto.application.Application().connect(handle=win._hWnd).top_window().set_focus()
     win.activate() #윈도우 활성화
 
-def push_btn(typing):
-    time.sleep(random.uniform(0.10,0.1456))
-    pag.keyDown(typing)
-    time.sleep(random.uniform(0.256,0.356))
-    pag.keyUp(typing)
+#시작 행 가져오기
+def getStartRow():
+    no=1
+    while True:
+        cell = load_ws.cell(no,1)
+        if(cell.value == 1):
+            return(cell.row)
+        no+=1
 
-# bring_to_window()
 #항상 작동중인 스레드
 class always(threading.Thread):
     def __init__(self, name):
@@ -37,46 +44,78 @@ thread = always("child")
 thread.daemon = True
 thread.start()
 
+#엑셀파일 조작
+#읽기
+load_wb = load_workbook("./최재원.xlsx",data_only=True)
+load_ws = load_wb['Sheet1']
+#백업용 쓰기
+write_wb = Workbook()
+write_ws = write_wb.active
+
+i = getStartRow()-1
+bring_to_window()
+print("시작열은:%s열"% (i+1))
+#매크로 메인
 while True:
     load_count = 0
+    i+=1
 
-    img_add = pag.locateCenterOnScreen("./img/add.png",confidence=0.9)
-    if(img_add):
-        print("추가..")
-        pag.click(img_add.x,img_add.y-13)
-        time.sleep(0.5)
+    team = str(load_ws.cell(i,4).value)
+    StudentID = str(load_ws.cell(i,5).value)
+    num = str(load_ws.cell(i,1).value)
+    name = str(load_ws.cell(i,6).value)
+
+    #데이터 다읽으면 종료
+    if(StudentID=="None"):
+        print("끝")
+        break
+    
+    while True:
+        img_add = pag.locateCenterOnScreen("./img/add.png",confidence=0.9)
+        if (img_add): 
+            break
+        print("!처음화면으로 가주세요!")
+    
+    print("추가..")
+    pag.click(img_add.x,img_add.y-13)
+    time.sleep(0.5)
+    while True:
+        print("로딩중..")
+        if (load_count<2):
+            img_StudentID = pag.locateCenterOnScreen("./img/StudentID.png",confidence=0.9)
+            load_count+=1
+        img_loading = pag.locateCenterOnScreen("./img/loading.png",confidence=0.9)
+        if(not img_loading):
+            break
+    print("로딩완료..")
+    print("학생조회")
+    if(img_StudentID):
+        pag.click(img_StudentID.x+50,img_StudentID.y)
+        print("입력필요")
+        # pag.typewrite(StudentID)
+        pyperclip.copy(StudentID)
+        pag.hotkey("ctrl","v")
+        pag.press("enter")
+        print("입력완료")
+        load_count=0
         while True:
             print("로딩중..")
             if (load_count<2):
-                img_name = pag.locateCenterOnScreen("./img/name.png",confidence=0.9)
+                img_check = pag.locateCenterOnScreen("./img/check.png",confidence=0.9)
                 load_count+=1
             img_loading = pag.locateCenterOnScreen("./img/loading.png",confidence=0.9)
             if(not img_loading):
                 break
-        print("로딩완료..")
-        print("학생조회")
-        if(img_name):
-            pag.click(img_name.x+50,img_name.y)
-            print("입력필요")
-            while True:
-                if(keyboard.is_pressed("space")):
-                    break
-            print("입력완료")
-            img_check = pag.locateCenterOnScreen("./img/check.png",confidence=0.9)
-            if (img_check):
-                pag.click(img_check)
-                time.sleep(0.3)
-                print("등록완료")
-                img_team = pag.locateCenterOnScreen("./img/team.png",confidence=0.9)
-                if (img_team):
-                    pag.click(img_team.x-10,img_team.y)
-                    print("팀입력")
-                    while True:
-                        if keyboard.is_pressed("1"):
-                            break
-                        elif keyboard.is_pressed("2"):
-                            break
-                        elif keyboard.is_pressed("3"):
-                            break
-                        elif keyboard.is_pressed("4"):
-                            break
+        # img_check = pag.locateCenterOnScreen("./img/check.png",confidence=0.9)
+        if (img_check):
+            pag.click(img_check)
+            time.sleep(0.3)
+            print("등록완료")
+            img_team = pag.locateCenterOnScreen("./img/team.png",confidence=0.9)
+            if (img_team):
+                print("팀입력")
+                pag.click(img_team.x-12,img_team.y)
+                pag.typewrite(team)
+    write_ws.cell(i,1,num)
+    write_ws.cell(i,2,name)        
+    write_wb.save("./backup.xlsx")
--- SRC_Center_UploadMento/test.py
+++ SRC_Center_UploadMento/test.py
@@ -2,7 +2,7 @@ import time
 from openpyxl import load_workbook
 from openpyxl import Workbook
 
-load_wb = load_workbook("./2022mento.xlsx",data_only=True)
+load_wb = load_workbook("./최재원.xlsx",data_only=True)
 
 load_ws = load_wb['Sheet1']
 
@@ -10,16 +10,24 @@ write_wb = Workbook()
 write_ws = write_wb.create_sheet('생성시트')
 write_ws = write_wb.active
 
-write_ws['J1'] = "입력"
 i=1
+no=1
 while True:
-    print(load_ws.cell(i,4).value)
-    print(load_ws.cell(i,5).value)
-    if(load_ws.cell(i,4).value==None):
+    cell = load_ws.cell(no,1)
+    if(cell.value == 1):
+        print(cell.row)
         break
-    # time.sleep(0.1)
-    for cell in load_ws.rows[i]:
-        write_ws.append(cell.value)
-    i+=1
+    no+=1
+
+    # print(load_ws.cell(i,4).value)
+    # print(load_ws.cell(i,5).value)
+    # if(load_ws.cell(i,4).value==None):
+    #     break
+    # # time.sleep(0.1)
+    
+    # write_ws.cell(i,1,load_ws.cell(i,1).value)
+    # write_ws.cell(i,2,load_ws.cell(i,6).value)
+
+    # i+=1
 
 write_wb.save("./backup.xlsx")
